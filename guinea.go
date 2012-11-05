9f2a6eedd73635e2949b56219176f76ce96808c0
Oskar Thoren <o.thoren@gmail.com>
2012-11-05 18:30:01 -0500

    Remove everything. Echo characters with termbox and ctrl-C cancel.

--- guinea.go
+++ guinea.go
@@ -1,57 +1,26 @@
 package main
 
 import (
-  "net"
-  "fmt"
-  termbox "github.com/nsf/termbox-go"
+	"fmt"
+	termbox "github.com/nsf/termbox-go"
 )
 
-func drawLine() {
-  w, h := termbox.Size()
-  for i := 0; i < w; i++ {
-    termbox.SetCell(i, h-2, ' ', termbox.ColorBlack,
-    termbox.ColorWhite)
-  }
-  termbox.Flush()
-}
-
-func handleInput(quit chan bool) {
-  for {
-    e := termbox.PollEvent()
-    if e.Ch == 0 {
-      switch e.Key {
-      case termbox.KeyCtrlC:
-        quit <- true
-        return
-      }
-    }
-
-    if e.Ch != 0 {
-      fmt.Println("letter")
-      return
-    }
-  }
-}
-
 func main() {
-  ln, _ := net.Listen("tcp", ":3000")
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
 
-  conn, _ := ln.Accept()
-
-  err := termbox.Init()
-  if err != nil {
-    fmt.Println("ERROR:", err)
-  }
-
-  defer termbox.Close()
-
-  //hist = []rune{}
-  //go manager.Listen()
-
-  quit := make(chan bool, 1)
-  go handleInput(quit)
-
-  <-quit
-
-  conn.Close()
+	for {
+		e := termbox.PollEvent()
+		if e.Ch == 0 {
+			switch e.Key {
+			case termbox.KeyCtrlC:
+				return
+			}
+		} else {
+			fmt.Print(string(e.Ch))
+		}
+	}
 }
