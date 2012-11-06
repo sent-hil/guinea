a71b67d9aae09f6593ccc19499f5fa356e4bc65f
Oskar Thoren <o.thoren@gmail.com>
2012-11-06 11:22:06 -0500

    Echo server with [8]byte buffer.

--- server.go
+++ server.go
@@ -7,11 +7,34 @@ import (
 
 func main() {
 	ln, _ := net.Listen("tcp", ":3000")
-	conn, err := ln.Accept()
-	
-	if err != nil {
-		fmt.Println("ERR")
-	}
 
-	conn.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// why continue?
+			fmt.Println("ERR")
+		}
+		go handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	// close connection on exit
+	defer conn.Close()
+	
+	var buf [8]byte
+	for {
+		// read buffer
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			return
+		}
+		
+		// write buffer
+		fmt.Println(buf)
+		_, err2 := conn.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+	}
 }
