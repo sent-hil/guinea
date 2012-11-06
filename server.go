f217e9793f1aa1ec4123489d6d51108f4b7da65a
Oskar Thoren <o.thoren@gmail.com>
2012-11-06 12:40:47 -0500

    print remoteaddr

--- server.go
+++ server.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	ln, _ := net.Listen("tcp", ":3000")
 
+	// server loop
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -15,17 +16,28 @@ func main() {
 			fmt.Println("ERR")
 		}
 		go handleClient(conn)
+
+		// inside handleClient, once connect,
+		// print remoteAddr
+
+		// here read handleclient channel
 	}
 }
 
 func handleClient(conn net.Conn) {
 	// close connection on exit
 	defer conn.Close()
-	
+
+	// print client info
+	fmt.Println(conn.RemoteAddr())
+
+	// move away / no use?
 	// create a channel for message
 	message := make(chan string)
-	
+
 	var buf [8]byte
+
+	// client loop
 	for {
 		// read buffer
 		n, err := conn.Read(buf[0:])
@@ -34,7 +46,7 @@ func handleClient(conn net.Conn) {
 		}
 
 		// send buffer to channel
-		go func() {message <- string(buf[0:n])}()
+		go func() { message <- string(buf[0:n]) }()
 
 		msg := <-message
 		fmt.Println(msg)
